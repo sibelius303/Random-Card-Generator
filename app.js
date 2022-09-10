51eb683dfabb1fcf4ad9f4f3d15e14b305930039
Cesar Vallenilla <cesarev17@gmail.com>
2022-09-10 18:13:26 -0300

    Version final, con colores

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const icono = document.querySelector('.icono');
 const icono1 = document.querySelector('#icono1');
 const numero = document.querySelector('#numero');
 
-const iconoText = ['♥','♦','♣','♠'];
+const iconoCorazon = '♥';
+const iconoDiamante = '♦';
+
+const iconoText = [iconoCorazon,iconoDiamante,'♣','♠'];
 const numeroText = ['3','5','6','9','A']
 const iconoRandom = Math.floor(Math.random()*iconoText.length);
 
@@ -10,6 +13,18 @@ function randomNumero (){
     let numeroRandom = Math.floor(Math.random()*numeroText.length);
     return numeroText[numeroRandom];
 }
+
+if (iconoText[iconoRandom] === iconoCorazon) {
+    icono.style.color = 'red';
+    icono1.style.color = 'red';
+} else if (iconoText[iconoRandom] === iconoDiamante) {
+    icono.style.color = 'red';
+    icono1.style.color = 'red';
+} else {
+    icono.style.color = 'black';
+    icono1.style.color = 'black';
+}
+
 icono.innerHTML=iconoText[iconoRandom];
 icono1.innerHTML=iconoText[iconoRandom];
 numero.innerHTML=randomNumero();
